b10e251932c0cd9a0f5d46a4ef3ad2de9680752f
Mauricio Mani <35645665+mauriciomani@users.noreply.github.com>
2025-12-04 23:21:19 -0600

    Add tool change and Z axis homing commands
    Added commands for tool change and Z axis homing.

--- flatcam_gcode_generator.py
+++ flatcam_gcode_generator.py
@@ -9,6 +9,13 @@ M420 S1
 G0 Z10 F1000         
 G0 X34 Y65 F3000 
 
+0 Z55 F500 ; Raise Z axis 35mm make sure you have enough room
+M00 ; Pause and wait for the tool change
+G28 Z ; Home the Z axis to establish the new Z position
+G00 Z5.0000 F500 ; Lift off touchplate
+M00 ; Pause to remove the touchplate
+
+
 # Remove G21
 # Remove G94
 # Remove M5
